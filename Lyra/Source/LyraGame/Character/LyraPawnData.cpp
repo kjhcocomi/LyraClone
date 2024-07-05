c04288feb9db933a0c8f14a722bb1cd305fc886d
kjhcocomi <981028_@naver.com>
2024-07-05 13:48:37 +0900

    Asset Manager

---
+++ Lyra/Source/LyraGame/Character/LyraPawnData.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LyraPawnData.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(LyraPawnData)
+
+ULyraPawnData::ULyraPawnData(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PawnClass = nullptr;
+
+	//tmp
+	//InputConfig = nullptr;
+	//DefaultCameraMode = nullptr;
+}
+
